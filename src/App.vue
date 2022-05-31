810a77afd88304fd6023236211669b7f918edbcb
Sonny Brown <lowlifearcade@gmail.com>
2022-05-30 21:43:53 -0700

    added game logic

--- src/App.vue
+++ src/App.vue
@@ -1,131 +1,210 @@
 <template>
-    <div class="game-message">
-        <div>
-            {{ gameState }}
-        </div>
-        <div>
-            {{ gameSubState }}
-        </div>
-        <p>
-            {{ message }}
-        </p>
+  <div class="game-message">
+    <!-- <div>
+      {{ storyState }}
     </div>
-    {{ prompt }}
-    <input type="text" v-model="characterAction" @keyup.enter="submitAction" />
+    <div>
+      {{ subStoryState }}
+    </div> -->
+    <p>
+      {{ message }}
+    </p>
+  </div>
+  {{ prompt }}
+  <input
+    autofocus
+    ref="input"
+    type="text"
+    :disabled="disabled"
+    v-model="characterAction"
+    @keyup.enter="dispatch"
+  />
 </template>
 
 <script>
-    const CHARACTER_STATE = { IDLE: 'IDLE', ACTION: 'ACTION' };
-    // game state : title, active, game over
-    // story state: episodes and how they branch
-    // episode state: DOOR episode has certain actions and messages it displays
-    // character state: tired, scared, angry, idle, active
-    const STORY_STATE = {
-        initialState: 'DOOR',
-        DOOR: {
-            actions: {
-                enterCeler() {
-                    this.storyState = 'CELER';
-                },
-            },
-        },
-    };
-    const GAME_STATE = {
-        state: 'DOOR',
-        actions: {
-            DOOR: {
-                state: 'beforeKnock',
-                actions: {
-                    beforeKnock: {
-                        knock() {
-                            console.log(this.knock);
+// character state: tired, scared, angry, idle, active
+const CHARACTER_STATE = { IDLE: "IDLE", ACTION: "ACTION" };
+const STORY_STATE = {
+  DOOR: {
+    value: "DOOR",
+    subStates: {
+      beforeKnock: { value: "before the knock" },
+      afterKnock: { value: "after the knock" },
+      someoneAtDoor: {value: "someone is at the door, but the door has not opened"}
+    },
+  },
+};
+// const STORY_STATE = {
+//     DOOR: {
+//         initState: 'beforeKnock',
+//         actions: {
+//             beforeKnock: {
+//                 knock() {
+//                     this.message = 'You knocked'
+//                 },
+//                 inspect() {
+//                     this.message = 'You looked around, but found nothing';
+//                 },
+//             },
+//             afterKnock: {
+//                 lookAround: 'look around',
+//                 runAway: 'run away',
+//             },
+//         },
+//     },
+//     dispatch(actionName) {
+//         const action = this.storyStates[this.gameState].actions[this.subStoryState][actionName];
 
-                            this.knock();
-                        },
-                        inspect() {
-                            this.message = 'You looked around, but found nothing';
-                        },
-                    },
-                    afterKnock: {
-                        lookAround: 'look around',
-                        runAway: 'run away',
-                    },
-                },
-            },
-        },
-        dispatch(actionName) {
-            // const action = this.state[this.gameState].actions[this.gameSubState][actionName];
-            const action =
-                this.actions[this.state].actions[this.actions[this.state].state][actionName];
-            // console.log(this.actions[this.state].actions[this.actions[this.state].state][actionName]);
-            if (action) {
-                action.call(this);
-            } else {
-                this.prompt = "Unsure what you're trying to do";
-            }
-        },
+//         if (action) {
+//             action.call(this);
+//         } else {
+//             this.prompt = "Unsure what you're trying to do";
+//         }
+//     },
+// };
+export default {
+  data() {
+    return {
+      characterState: CHARACTER_STATE.IDLE,
+      storyState: STORY_STATE.DOOR.value,
+      subStoryState: STORY_STATE.DOOR.subStates.beforeKnock.value,
+      message: "You see a door",
+      prompt: "What will you do?",
+      characterAction: "",
+      disabled: false,
+      //   stateMachine: { ...GAME_STATE },
     };
-    export default {
-        data() {
-            return {
-                characterState: CHARACTER_STATE.IDLE,
-                gameState: GAME_STATE.state,
-                gameSubState: GAME_STATE.actions.DOOR.state,
-                message: 'You see a door',
-                prompt: 'What will you do?',
-                characterAction: '',
-                stateMachine: { ...GAME_STATE },
-            };
-        },
-        methods: {
-            submitAction(e) {
-                if (this.gameState === GAME_STATE.state)
-                    if ((this.characterState = CHARACTER_STATE.IDLE)) {
-                        if (this.characterAction == 'knock') {
-                            this.stateMachine.dispatch('knock');
-                        }
-                        this.characterAction = '';
-                    }
-                this.characterAction = '';
-            },
-            knock() {
-                console.log('reached');
-                this.gameSubState = 'afterKnock';
-                this.message = 'You knocked';
+  },
+  methods: {
+    submitAction(e) {
+      this.stateMachine.dispatch(e.target.value);
+      this.characterAction = "";
+    },
+    focus(){
+        this.$nextTick(function() {
+            this.$refs.input.focus()
+        })
+    },
+    knock() {
+      console.log("reached");
+      //   this.gameSubState = "afterKnock";
+      this.message = "You knocked";
+      setTimeout(() => {
+        this.message = "There was no answer";
+        setTimeout(() => {
+          this.message = "You hear a scream";
+          this.subStoryState = STORY_STATE.DOOR.subStates.afterKnock.value;
+          // new if statements available
+        }, 3000);
+      }, 2000);
+      //   this.character_action = "";
+    },
+    dispatch(e) {
+        this.disabled = true
+      let actionName = e.target.value;
+      console.log(actionName);
+      this.characterAction = "";
+      this.stateReducer(actionName);
+      // const action =
+      //     this.storyStates[this.gameState].actions[this.subStoryState][actionName];
+      // if (action) {
+      //     action.call(this);
+      // } else {
+      //     this.prompt = "Unsure what you're trying to do";
+      // }
+    },
+    stateReducer(actionName) {
+      if ((this.storyState = STORY_STATE.DOOR.value)) {
+        if (
+          this.subStoryState === STORY_STATE.DOOR.subStates.beforeKnock.value
+        ) {
+          switch (true) {
+            case actionName.includes("knock"):
+              this.message = "You knocked";
+              setTimeout(() => {
+                this.message = "There was no answer";
                 setTimeout(() => {
-                    this.message = 'There was no answer';
-                    setTimeout(() => {
-                        this.message = 'You hear a scream';
-                        // new if statements available
-                    }, 3000);
-                }, 2000);
-                this.character_action = '';
-            },
-        },
-    };
+                  this.message = "You hear a scream";
+                  this.subStoryState =
+                    STORY_STATE.DOOR.subStates.afterKnock.value;
+                    this.disabled = false;
+                    this.focus()
+                  // new if statements available
+                }, 3000);
+              }, 2000);
+              break;
+
+            default:
+                this.disabled = false
+              break;
+          }
+        }
+
+        if (
+          this.subStoryState === STORY_STATE.DOOR.subStates.afterKnock.value
+        ) {
+          switch (true) {
+            case actionName.includes("knock"):
+              this.message = "You knocked again...";
+              setTimeout(() => {
+                this.message = "The sound of footsteps down a hallway";
+                setTimeout(() => {
+                  this.message = "The door handle jiggles";
+                  // new if statements available
+                  setTimeout(() => {
+                      this.message = "And jiggles..."
+                      this.subStoryState = STORY_STATE.DOOR.subStates.someoneAtDoor.value
+                      this.disabled = false
+                      this.focus()
+                  }, 2000);
+                }, 3000);
+              }, 3000);
+              break;
+
+            default:
+                this.disabled = false
+              break;
+          }
+        }
+
+        if (this.subStoryState === STORY_STATE.DOOR.subStates.someoneAtDoor.value) {
+            switch (true) {
+                case actionName.includes('run'):
+                    this.message = "you ran away..."
+                    break;
+            
+                default:
+                    this.disabled = false
+                    break;
+            }
+        }
+      }
+    },
+  },
+};
 </script>
 
 <style>
-    #app {
-        font-family: Avenir, Helvetica, Arial, sans-serif;
-        -webkit-font-smoothing: antialiased;
-        -moz-osx-font-smoothing: grayscale;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-        color: #2c3e50;
-        margin-top: 60px;
-    }
-    .game-message {
-        color: white;
-        width: 100%;
-        padding: 50px;
-        margin: 20px;
-        background: black;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        flex-direction: column;
-    }
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.game-message {
+  color: white;
+  width: 100%;
+  padding: 50px;
+  margin: 20px;
+  background: black;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+}
 </style>
