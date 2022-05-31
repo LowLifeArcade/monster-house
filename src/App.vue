e984fac37a9010b6822511cde706b0c8ed62e706
Sonny Brown <lowlifearcade@gmail.com>
2022-05-30 22:04:02 -0700

    Added more logic

--- src/App.vue
+++ src/App.vue
@@ -6,12 +6,16 @@
     <div>
       {{ subStoryState }}
     </div> -->
+
     <p>
       {{ message }}
     </p>
   </div>
-  {{ prompt }}
+  <div v-if="gameState === 'IN_PROGRESS'">
+    {{ prompt }}
+  </div>
   <input
+    v-if="gameState === 'IN_PROGRESS'"
     autofocus
     ref="input"
     type="text"
@@ -19,18 +23,27 @@
     v-model="characterAction"
     @keyup.enter="dispatch"
   />
+  <div v-if="gameState === 'START'">
+    <button @click="startGame">Start Game?</button>
+  </div>
+  <div v-if="gameState === 'END'">
+    <button @click="startGame">Try Again?</button>
+  </div>
 </template>
 
 <script>
 // character state: tired, scared, angry, idle, active
 const CHARACTER_STATE = { IDLE: "IDLE", ACTION: "ACTION" };
+const GAME_STATE = { START: "START", IN_PROGRESS: "IN_PROGRESS", END: "END" };
 const STORY_STATE = {
   DOOR: {
     value: "DOOR",
     subStates: {
       beforeKnock: { value: "before the knock" },
       afterKnock: { value: "after the knock" },
-      someoneAtDoor: {value: "someone is at the door, but the door has not opened"}
+      someoneAtDoor: {
+        value: "someone is at the door, but the door has not opened",
+      },
     },
   },
 };
@@ -65,10 +78,11 @@ const STORY_STATE = {
 export default {
   data() {
     return {
+      gameState: GAME_STATE.START,
       characterState: CHARACTER_STATE.IDLE,
       storyState: STORY_STATE.DOOR.value,
       subStoryState: STORY_STATE.DOOR.subStates.beforeKnock.value,
-      message: "You see a door",
+      message: "Monster House",
       prompt: "What will you do?",
       characterAction: "",
       disabled: false,
@@ -80,10 +94,17 @@ export default {
       this.stateMachine.dispatch(e.target.value);
       this.characterAction = "";
     },
-    focus(){
-        this.$nextTick(function() {
-            this.$refs.input.focus()
-        })
+    focus() {
+      this.$nextTick(function () {
+        this.$refs.input.focus();
+      });
+    },
+    startGame() {
+      this.gameState = "IN_PROGRESS";
+      this.storyState = STORY_STATE.DOOR.value;
+      this.subStoryState = STORY_STATE.DOOR.subStates.beforeKnock.value;
+      (this.message = "You see a door"), (this.disabled = false);
+      this.focus();
     },
     knock() {
       console.log("reached");
@@ -100,7 +121,7 @@ export default {
       //   this.character_action = "";
     },
     dispatch(e) {
-        this.disabled = true
+      this.disabled = true;
       let actionName = e.target.value;
       console.log(actionName);
       this.characterAction = "";
@@ -127,15 +148,20 @@ export default {
                   this.message = "You hear a scream";
                   this.subStoryState =
                     STORY_STATE.DOOR.subStates.afterKnock.value;
-                    this.disabled = false;
-                    this.focus()
+                  this.disabled = false;
+                  this.focus();
                   // new if statements available
                 }, 3000);
               }, 2000);
               break;
-
+            case actionName.includes("run"):
+              this.message = "You ran away...";
+              setTimeout(() => {
+                this.gameState = GAME_STATE.END;
+              }, 1000);
+              break;
             default:
-                this.disabled = false
+              this.disabled = false;
               break;
           }
         }
@@ -152,31 +178,37 @@ export default {
                   this.message = "The door handle jiggles";
                   // new if statements available
                   setTimeout(() => {
-                      this.message = "And jiggles..."
-                      this.subStoryState = STORY_STATE.DOOR.subStates.someoneAtDoor.value
-                      this.disabled = false
-                      this.focus()
+                    this.message = "And jiggles...";
+                    this.subStoryState =
+                      STORY_STATE.DOOR.subStates.someoneAtDoor.value;
+                    this.disabled = false;
+                    this.focus();
                   }, 2000);
                 }, 3000);
               }, 3000);
               break;
 
             default:
-                this.disabled = false
+              this.disabled = false;
               break;
           }
         }
 
-        if (this.subStoryState === STORY_STATE.DOOR.subStates.someoneAtDoor.value) {
-            switch (true) {
-                case actionName.includes('run'):
-                    this.message = "you ran away..."
-                    break;
-            
-                default:
-                    this.disabled = false
-                    break;
-            }
+        if (
+          this.subStoryState === STORY_STATE.DOOR.subStates.someoneAtDoor.value
+        ) {
+          switch (true) {
+            case actionName.includes("run"):
+              this.message = "You ran away...";
+              setTimeout(() => {
+                this.gameState = GAME_STATE.END;
+              }, 1000);
+              break;
+
+            default:
+              this.disabled = false;
+              break;
+          }
         }
       }
     },
